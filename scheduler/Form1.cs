0556fd0df0fc7a07902a411bf36f014da8083a9d
OverLord561 <yurapuk452@gmail.com>
2018-01-25 15:46:01 +0200

    auto resize

--- scheduler/Form1.cs
+++ scheduler/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -22,38 +22,58 @@ namespace scheduler
 
         }
 
+       
+
+        private void PrintRecursive(Control control, int width)
+        {
+            foreach (Control c in control.Controls)
+            {
+                if (c.GetType() == typeof(TabPage) || c.GetType() == typeof(DataGridView) )
+                {
+                    c.Width = width;
+                    PrintRecursive(c, width);
+
+                }
+
+            }
+        }
+
+        private IEnumerable<Control> GetAll(Control control, Type type)
+        {
+            var controls = control.Controls.Cast<Control>();
+
+            return controls.SelectMany(ctrl => GetAll(ctrl, type))
+                                      .Concat(controls)
+                                      .Where(c => c.GetType() == type);
+        }
+
+
         private void Scheduler_Resize(object sender, EventArgs e)
         {
-            this.dataGridView1.Width = (sender as Scheduler).ClientSize.Width;
-            this.dataGridView2.Width = (sender as Scheduler).ClientSize.Width;
-            this.dataGridView3.Width = (sender as Scheduler).ClientSize.Width;
-            this.dataGridView4.Width = (sender as Scheduler).ClientSize.Width;
-            this.dataGridView5.Width = (sender as Scheduler).ClientSize.Width;
-            this.dataGridView6.Width = (sender as Scheduler).ClientSize.Width;
-            this.dataGridView7.Width = (sender as Scheduler).ClientSize.Width;
-            this.dataGridView8.Width = (sender as Scheduler).ClientSize.Width;
-            this.dataGridView9.Width = (sender as Scheduler).ClientSize.Width;
-            this.dataGridView10.Width = (sender as Scheduler).ClientSize.Width;
-            this.dataGridView11.Width = (sender as Scheduler).ClientSize.Width;
-            this.dataGridView12.Width = (sender as Scheduler).ClientSize.Width;
-            this.dataGridView13.Width = (sender as Scheduler).ClientSize.Width;
-            this.tabControl1.Width = (sender as Scheduler).ClientSize.Width;
-            this.tabControl2.Width = (sender as Scheduler).ClientSize.Width;
-            this.tabControl3.Width = (sender as Scheduler).ClientSize.Width;
-            this.ControlLists.Width = (sender as Scheduler).ClientSize.Width;
-            this.groupBox1.Width = (sender as Scheduler).ClientSize.Width;
-            this.groupBox2.Width = (sender as Scheduler).ClientSize.Width;
-            this.groupBox3.Width = (sender as Scheduler).ClientSize.Width;
-            this.groupBox4.Width = (sender as Scheduler).ClientSize.Width;
-            this.groupBox5.Width = (sender as Scheduler).ClientSize.Width;
-            this.groupBox6.Width = (sender as Scheduler).ClientSize.Width;
-            this.groupBox7.Width = (sender as Scheduler).ClientSize.Width;
-            this.groupBox8.Width = (sender as Scheduler).ClientSize.Width;
-            this.groupBox9.Width = (sender as Scheduler).ClientSize.Width;
-            this.groupBox10.Width = (sender as Scheduler).ClientSize.Width;
-            this.groupBox11.Width = (sender as Scheduler).ClientSize.Width;
-            this.groupBox12.Width = (sender as Scheduler).ClientSize.Width;
-            this.groupBox13.Width = (sender as Scheduler).ClientSize.Width;
+            if (sender == null) return;
+            var width = Screen.PrimaryScreen.WorkingArea.Width;
+
+            this.tabControl1.Width = width;
+                     
+
+            var controls = GetAll(this.tabControl1, typeof(TabControl));
+            foreach (Control c in controls)
+            {
+                c.Width = width;
+            }
+            var grids = GetAll(this.tabControl1, typeof(DataGridView));
+            foreach (Control c in grids)
+            {
+                c.Width = width;
+            }
+
+            var groupBox = GetAll(this.tabControl1, typeof(GroupBox));
+            foreach (Control c in groupBox)
+            {
+                c.Width = width;
+            }
+
+            
         }
     }
 }
